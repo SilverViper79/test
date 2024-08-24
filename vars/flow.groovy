58475c59127799c41c245791cb2ba6c53c616d2e
Full Name <vishalbaghla@Vishals-MacBook-Pro.local>
2024-08-25 01:28:29 +0530

    Add CI/CD pipeline configuration

--- vars/flow.groovy
+++ vars/flow.groovy
@@ -1,3 +1,5 @@
+import static org.jenkinsci.plugins.pipeline.modeldefinition.Utils.readYaml
+
 def setEnvVarsFromYaml(String envFile) {
     def envVars = readYaml file: envFile
     envVars.each { key, value ->
