03364cef19c66032ac36785a954c0e49b057cd2a
Full Name <vishalbaghla@Vishals-MacBook-Pro.local>
2024-08-28 05:13:03 +0530

    Add CI/CD pipeline configuration

--- vars/helm.groovy
+++ vars/helm.groovy
@@ -24,7 +24,7 @@ def helmStages(Map pipelineMetadata) {
 
 def helmLint(String helmChartPath) {
     try {
-        sh "helm lint ${helmChartPath}"
+        sh "/usr/local/bin/helm lint ${helmChartPath}"
         echo "Helm linting completed successfully."
     } catch (Exception e) {
         error "Helm lint failed for chart at ${helmChartPath}.\n${e.message}"
