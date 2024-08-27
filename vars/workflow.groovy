b8e0e9dac347b5fc636a375dc03b0be32fde1955
Full Name <vishalbaghla@Vishals-MacBook-Pro.local>
2024-08-28 04:07:57 +0530

    Add CI/CD pipeline configuration

--- vars/workflow.groovy
+++ vars/workflow.groovy
@@ -1,15 +1,16 @@
 def call(){
     node {
         cleanWs()
-        sh "git clone https://github.com/trainingdevsecops/Dummy.git"
-        echo "---"
-        def pipelineConfigPath = "Dummy/.pipeline/config.yaml"
-        def envFilePath = 'Dummy/config/env.yaml'
-        def helmConfigPath = 'Dummy/config/values.yaml'
+        stage('init - pipeline') {
+            sh "git clone https://github.com/trainingdevsecops/Dummy.git"
+            echo "---"
+            def pipelineConfigPath = "Dummy/.pipeline/config.yaml"
+            def envFilePath = 'Dummy/config/env.yaml'
+            def helmConfigPath = 'Dummy/config/values.yaml'
 
-        def pipelineMetadata = config(pipelineConfigPath)
+            def pipelineMetadata = config(pipelineConfigPath)
 
-        try {
+            try {
                 setEnvVarsFromYaml(envFilePath)
                 parseYaml(helmConfigPath)
                 echo "-------"
@@ -17,16 +18,17 @@ def call(){
                 echo "-------"
 
 
-            if (pipelineMetadata["pipeline"]["deploy"]["k8s"]) {
-                echo "k8s"
-                stage(pipelineMetadata)
-            }
+                if (pipelineMetadata["pipeline"]["deploy"]["k8s"]) {
+                    echo "k8s"
+                    stage(pipelineMetadata)
+                }
 
-            if (pipelineMetadata["pipeline"]["deploy"]["lambda"]) {
-                echo "Lambda"
+                if (pipelineMetadata["pipeline"]["deploy"]["lambda"]) {
+                    echo "Lambda"
+                }
+            } catch (Exception exception) {
+                throw exception
             }
-        } catch (Exception exception) {
-            throw exception
         }
     }
 }
