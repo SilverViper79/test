976860f80ee06a4f5e651e1f6506c81b7dde4e70
Full Name <vishalbaghla@Vishals-MacBook-Pro.local>
2024-08-28 04:10:54 +0530

    Add CI/CD pipeline configuration

--- vars/workflow.groovy
+++ vars/workflow.groovy
@@ -1,34 +1,32 @@
 def call(){
     node {
         cleanWs()
-        stage('init - pipeline') {
-            sh "git clone https://github.com/trainingdevsecops/Dummy.git"
-            echo "---"
-            def pipelineConfigPath = "Dummy/.pipeline/config.yaml"
-            def envFilePath = 'Dummy/config/env.yaml'
-            def helmConfigPath = 'Dummy/config/values.yaml'
+        sh "git clone https://github.com/trainingdevsecops/Dummy.git"
+        echo "---"
+        def pipelineConfigPath = "Dummy/.pipeline/config.yaml"
+        def envFilePath = 'Dummy/config/env.yaml'
+        def helmConfigPath = 'Dummy/config/values.yaml'
 
-            def pipelineMetadata = config(pipelineConfigPath)
+        def pipelineMetadata = config(pipelineConfigPath)
 
-            try {
-                setEnvVarsFromYaml(envFilePath)
-                parseYaml(helmConfigPath)
-                echo "-------"
-                sh "printenv"
-                echo "-------"
+        try {
+            setEnvVarsFromYaml(envFilePath)
+            parseYaml(helmConfigPath)
+            echo "-------"
+            sh "printenv"
+            echo "-------"
 
 
-                if (pipelineMetadata["pipeline"]["deploy"]["k8s"]) {
-                    echo "k8s"
-                    stage(pipelineMetadata)
-                }
-
-                if (pipelineMetadata["pipeline"]["deploy"]["lambda"]) {
-                    echo "Lambda"
-                }
-            } catch (Exception exception) {
-                throw exception
+            if (pipelineMetadata["pipeline"]["deploy"]["k8s"]) {
+                echo "k8s"
+                stage(pipelineMetadata)
             }
+
+            if (pipelineMetadata["pipeline"]["deploy"]["lambda"]) {
+                echo "Lambda"
+            }
+        } catch (Exception exception) {
+            throw exception
         }
     }
 }
