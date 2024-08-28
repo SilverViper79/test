b1166bcd93e85528b4837166ca519b05ceb4dada
Full Name <vishalbaghla@Vishals-MacBook-Pro.local>
2024-08-29 03:31:43 +0530

    Add CI/CD pipeline configuration

--- vars/helm.groovy
+++ vars/helm.groovy
@@ -14,6 +14,6 @@ def helmStages(Map pipelineMetadata) {
 
     stage('Deploy') {
         echo "Deploy Stage"
-        utils.helmApply(pipelineMetadata['helmChartPath'], 'hello-world')
+        utils.helmApply(pipelineMetadata['helmChartPath'], 'hello-world', valuesFile = pipelineMetadata['helmConfigPath'])
     }
 }
