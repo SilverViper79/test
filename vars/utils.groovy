4363af6229f784eca3af642d363d72ffb775eace
Full Name <vishalbaghla@Vishals-MacBook-Pro.local>
2024-08-29 03:17:55 +0530

    Add CI/CD pipeline configuration

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -42,7 +42,7 @@ def helmLint(String helmChartPath) {
 def helmApply(String helmChartPath, String releaseName, String namespace = 'default', String valuesFile = null, boolean dryRun = false, boolean atomic = false, boolean wait = false, String timeout = '5m', boolean force = false) {
     try {
         sh """
-            sh '/usr/local/bin/helm lint ${helmChartPath}'
+            /usr/local/bin/helm lint ${helmChartPath}
             helm upgrade --install ${releaseName} ${helmChartPath} \
             --namespace ${namespace} \
             ${valuesFile ? '--values ${valuesFile}' : ''} \
