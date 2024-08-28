22b5f794aeb6f49243e017707b5b3b82d7ef9e63
Full Name <vishalbaghla@Vishals-MacBook-Pro.local>
2024-08-29 02:30:29 +0530

    Add CI/CD pipeline configuration

--- vars/helm.groovy
+++ vars/helm.groovy
@@ -11,14 +11,14 @@ def helmStages(Map pipelineMetadata) {
     stage('Helm Lint') {
         echo "Helm Lint Stage"
         echo "Helm Values Config: ${env.CHART_NAME}"
-        def helmChartPath = "examples/charts/${env.CHART_NAME}"
+        def helmChartPath = "Dummy/charts/${env.CHART_NAME}"
         helmLint(helmChartPath)
     }
 
     stage('Deploy') {
         echo "Deploy Stage"
         echo "Helm Values Config: ${pipelineMetadata}"  // Correct the reference to pipelineMetadata
-        // helmapply(pipelineMetadata)  // Uncomment when helmapply is implemented
+         helmapply(pipelineMetadata)  // Uncomment when helmapply is implemented
     }
 }
 
